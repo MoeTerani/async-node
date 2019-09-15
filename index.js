1c7c77e52d61e47912d21c30194cea7823d6a2f7
MoeTerani <moe.terani@gmail.com>
2019-09-15 22:16:35 +0200

    async-await

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 fs = require('fs');
-
 // This is callback hell  (Callback function inside another calllback function and ....)
 
 const superagent = require('superagent');
@@ -24,19 +23,37 @@ const writeFilePro = (file, data) => {
   });
 };
 
-// we can chain functions if they return a promise , it will async and they'll be more maintainable
-readFilePro(`${__dirname}/dog.txt`)
-  .then(data => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then(res => {
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
     console.log(res.body.message);
-    return writeFilePro('dog-img.txt', res.body.message);
-  })
-  .then(() => {
+    await writeFilePro('dog-img.txt', res.body.message);
     console.log('Random dog image saved to file!');
-  })
-  .catch(err => {
+  } catch (err) {
     console.log(err);
-  });
+  }
+};
+
+getDogPic();
+
+// // we can chain functions if they return a promise , it will async and they'll be more maintainable
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then(data => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then(res => {
+//     console.log(res.body.message);
+//     return writeFilePro('dog-img.txt', res.body.message);
+//   })
+//   .then(() => {
+//     console.log('Random dog image saved to file!');
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
