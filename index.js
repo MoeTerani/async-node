e29182ee0119d8336d6f11f947d573eb48b88970
MoeTerani <moe.terani@gmail.com>
2019-09-15 21:24:54 +0200

    using .get .then .catch

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ fs = require('fs');
 
 const superagent = require('superagent');
 
-fs.readFile('./dog.txt', function(err, data) {
+fs.readFile(`${__dirname}/dog.txt`, function(err, data) {
   if (err) return console.error(err);
   console.log(`Breed: ${data}`);
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
+    .then(res => {
       console.log(res.body.message);
 
-      fs.writeFile('dog-img.txt', res.body.message, err => {
+      fs.writeFile('./dog-img.txt', res.body.message, err => {
         if (err) return console.log(err.message);
-        console.log('Random dog image saved to ffile');
+        console.log('Random dog image saved to file');
       });
-    });
+    })
+    .catch(err => console.log(err.message));
 });
