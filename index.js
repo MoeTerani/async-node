02edf2cbf6cea0a167ab18bf29763ad57d0a13e0
MoeTerani <moe.terani@gmail.com>
2019-09-15 21:40:26 +0200

    create functions that return promises and chain them with .then()

--- index.js
+++ index.js
@@ -4,18 +4,39 @@ fs = require('fs');
 
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, function(err, data) {
-  if (err) return console.error(err);
-  console.log(`Breed: ${data}`);
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then(res => {
-      console.log(res.body.message);
+//Create a new promise
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 😢');
+      resolve(data);
+    });
+  });
+};
 
-      fs.writeFile('./dog-img.txt', res.body.message, err => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image saved to file');
-      });
-    })
-    .catch(err => console.log(err.message));
-});
+//Create a new promise
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject('Could not write file 😢');
+      resolve('success');
+    });
+  });
+};
+
+// we can chain functions if they return a promise , it will async and they'll be more maintainable
+readFilePro(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then(res => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
+  })
+  .catch(err => {
+    console.log(err);
+  });
