3498474e45efbfab1c3dbc4c845d78f1d9e5b4d9
MoeTerani <moe.terani@gmail.com>
2019-09-15 23:34:50 +0200

    execute multiple promises/async function at the same time using promise.all()

--- index.js
+++ index.js
@@ -28,18 +28,39 @@ const getDogPic = async () => {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res = await superagent.get(
+    const res1Pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
-    await writeFilePro('dog-img.txt', res.body.message);
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map(el => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
     console.log('Random dog image saved to file!');
   } catch (err) {
     console.log(err);
+
+    throw err;
   }
+  return '2: READY 🐶';
 };
 
-getDogPic();
+(async () => {
+  try {
+    console.log('1: Will get dog pics!');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  } catch (err) {
+    console.log('ERROR 💥');
+  }
+})();
 
 // // we can chain functions if they return a promise , it will async and they'll be more maintainable
 // readFilePro(`${__dirname}/dog.txt`)
